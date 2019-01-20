92ebfe6d7b49b8d6b3a742648ff29fad5f366afa
Andrew Partridge <2307565p@student.gla.ac.uk>
2019-01-20 19:21:36 +0000

    Added index.html for design, created static/images and templates/rango (Up to 4.3 TWD)

--- rango/views.py
+++ rango/views.py
@@ -5,9 +5,16 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-## sends a message to the client
+
 def index(request):
-    return HttpResponse("Rango says hey there partner! <br/> <a href='/rango/about/'>About</a>")
+    # Construct a dict. to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template
+    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+
+    # Return a rendered response to send to the client
+    # Make use of the shortcut function for ease of use
+    # Note that the first parameter is the template we wish to use
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return HttpResponse("Rango says here is the about page. <br/> <a href='/rango'/>Index</a>")
